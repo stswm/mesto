446277bd5a6f6a566cff5fafb8b73a3b399c38ba
Oleg Azadov <oazadov@gmail.com>
2021-12-20 23:03:55 +0300

    Fix: firstRevDone

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,28 @@
 const popupOpenButton = document.querySelector(".profile__edit-button");
 const popupCloseButton = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
+const defaultName = document.querySelector(".profile__name");
+const popupName = document.querySelector(".popup__name");
+const defaultAbout = document.querySelector(".profile__about");
+const popupAbout = document.querySelector(".popup__about");
+const formElement = document.querySelector(".popup__container");
+const nameInput = formElement.querySelector(".popup__name");
+const aboutInput = formElement.querySelector(".popup__about");
+const hearts = document.querySelectorAll(".element__heart");
 
 function togglePopup() {
   popup.classList.toggle("popup_opend");
+  nameInput.value = defaultName.textContent;
+  aboutInput.value = defaultAbout.textContent;
   // console.log('q');
 }
 
 function popupCloseOverlay(event) {
   if (event.target === event.currentTarget) {
     popup.classList.remove("popup_opend");
+    nameInput.value = defaultName.textContent;
+    aboutInput.value = defaultAbout.textContent;
+    
   }
   // console.log('w');
 }
@@ -19,47 +32,19 @@ popupCloseButton.addEventListener("click", togglePopup);
 
 popup.addEventListener("click", popupCloseOverlay);
 
-const hearts = document.querySelectorAll(".element__heart");
-//
 hearts.forEach((heart) => {
   heart.onclick = function () {
     heart.classList.toggle("element__heart_active");
-    // heart.classList.toggle("buttonEffect");
   };
 });
 
-//Имя попап
-const defaultName = document.querySelector(".profile__name");
-const popupName = document.querySelector(".popup__name");
-popupName.value = defaultName.textContent;
-// о себе попап
-const defaultAbout = document.querySelector(".profile__about");
-const popupAbout = document.querySelector(".popup__about");
-popupAbout.value = defaultAbout.textContent;
+// popupName.value = defaultName.textContent;
+// popupAbout.value = defaultAbout.textContent;
 
-
-const formElement = document.querySelector(".popup__container");
-const nameInput = formElement.querySelector(".popup__name");
-const jobInput = formElement.querySelector(".popup__about");
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  // Получите значение полей jobInput и nameInput из свойства value
-  
-  // Выберите элементы, куда должны быть вставлены значения полей
-  const defNa = document.querySelector(".profile__name");
-  const defAb = document.querySelector(".profile__about");
-  // Вставьте новые значения с помощью textContent
-  defNa.textContent = nameInput.value;
-  defAb.textContent = jobInput.value;
+  defaultAbout.textContent = aboutInput.value;
+  defaultName.textContent = nameInput.value;
   togglePopup();
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
-
-// const saveBTN = document.querySelector(".popup__save");
-// saveBTN.onclick = function submit(){
-//   defaultName.innerHTML = popupName.value;
-//   defaultAbout.innerHTML = popupAbout.value;
-//   togglePopup();
-// }
