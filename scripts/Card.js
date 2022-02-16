807b9a07278b663ecbd757ed954fde1019dd2476
Oleg Azadov <oazadov@gmail.com>
2022-02-16 18:08:13 +0300

    fix:delete function

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,4 +1,4 @@
-import { previewModal, previewImg, previewCaption, cardTemplateSelector } from "./constans.js";
+import { previewModal, previewImg, previewCaption } from "./constans.js";
 import { openPopup } from "./utils.js";
 
 export class Card {
@@ -9,31 +9,33 @@ export class Card {
   }
   _deleteHandler = (e) => {
     e.target.closest(".element").remove();
-  }
+    // this._cardElement.remove();
+  };
   _like = () => {
     this._likeBtn.classList.toggle("element__heart_active");
-  }
-  _preview = () =>  {
+  };
+  _preview = () => {
     previewImg.src = this._link;
     previewImg.alt = this._name;
     previewCaption.textContent = this._name;
     openPopup(previewModal);
-  }
+  };
   _setEventListeners = () => {
     this._likeBtn.addEventListener("click", this._like);
     this._deleteBtn.addEventListener("click", this._deleteHandler);
     this._cardImage.addEventListener("click", this._preview);
-  }
+  };
   _fillCard = () => {
     this._cardElement.querySelector(".element__pic").src = this._link;
     this._cardElement.querySelector(".element__pic").alt = this._name;
     this._cardElement.querySelector(".element__text").textContent = this._name;
-  }
+  };
 
-  createCard = () =>  {
+  createCard = () => {
     // this._cardElement = this._template.cloneNode(true);
-    this._cardElement = document.querySelector('.cardTemp').content.cloneNode(true);
-
+    this._cardElement = document
+      .querySelector(".cardTemp")
+      .content.cloneNode(true);
     this._deleteBtn = this._cardElement.querySelector(".element__delete");
     this._cardImage = this._cardElement.querySelector(".element__pic");
     this._likeBtn = this._cardElement.querySelector(".element__heart");
@@ -41,11 +43,5 @@ export class Card {
     this._fillCard();
     this._setEventListeners();
     return this._cardElement;
-  }
+  };
 }
-
-const cardTemp = document.querySelector(".cardTemp").content;
-// const cardTemplateSelector = '.cardTemp';
-// const cardIns = new Card({name: '', link: ''},cardTemplateSelector)
-// const cardElement = card.createCard()
-// list.prepend(cardElement)
